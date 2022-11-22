8c0858d470ecfd9d42c4d3bf1faa249d61fb9d8d
75310shubham <118808458+75310shubham@users.noreply.github.com>
2022-11-22 15:05:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let age = 48;
+let userName = "Gajodhar";
+let hobbies = ["Sport", "Cooking", "Reading"];
+
+// alert(age);
+// alert(56);
+// alert(userName);
+// alert(hobbies);
+// alert(hobbies[1]);
+
+// let job = { 
+//   title: "Gaurd", 
+//   place: "Dholakpur", 
+//   salary: "300" };
+let adultYear;
+// alert(job.salary);
+
+function calculateAdultYears(age) {
+ return age -18;
+}
+// calculateAdultYears();
+// console.log(adultYear);
+// alert(adultYear);
+// adultYear= calculateAdultYears(98);
+let priyanshuAdultYear=calculateAdultYears(18);
+alert([priyanshuAdultYear]);
+let kunalAdultYear=calculateAdultYears(19)
+alert(kunalAdultYear);
+
+// age = 68;
+// calculateAdultYears();
+// console.log(adultYear);
+// alert(adultYear);
+
+// let adultYear = age - 18;
+// alert(adultYear);
+
+// let person={
+//   name:'Babban', //Property
+//   greet(){      // Method
+//     // alert('Hello')
+//     console.log('Hello');
+//   }
+// };
+// person.greet();
---
+++ operation.js
@@ -0,0 +1,30 @@
+console.log(10+4);
+console.log(10-4);
+console.log(10*4);
+console.log(10/4);
+console.log(10%3);
+console.log(10+3-5*10);
+console.log(10.5*2);
+console.log(10+(5*2)-(4/2));
+
+let result=10+(5*2)-(4/2);
+console.log(result);
+result=10*4;
+result=result+1; console.log(result); 
+result++; console.log(result);
+result--; console.log(result);
+result+=5; console.log(result);
+result*=5; console.log(result);
+
+
+console.log('Pan'+'daya');
+console.log('Hardik'+' '+'Pandaya');
+console.log('pan'-'an');
+console.log('Virat'- 83);
+
+let userName= 'SuryaKumar';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies=['Scoring Run','To Win'];
+console.log(hobbies.length);
